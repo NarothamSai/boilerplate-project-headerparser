acb8b72af945a505151b24fee125335539cfb149
Narotham Sai <40522254+NarothamSai@users.noreply.github.com>
2021-07-10 11:45:08 +0000

    Add API for whoami

---
+++ spec/headerparser.spec.js
@@ -0,0 +1,12 @@
+describe("Suite for Header Parser Microsserivce,",function(){
+
+  describe("should return JSON object",function(){
+    it(":",function(){
+    
+    })
+
+    it("with your IP address in the ipaddress key")
+  })
+
+  
+})
